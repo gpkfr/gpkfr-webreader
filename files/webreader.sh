5cc6c189e9047a33c861b9704fc4e932c7ca4af0
Guillaume Pancak <guillaume@MacBook-Pro-de-Guillaume.local>
2014-06-05 11:06:24 +0200

    Fixed webreader.sh

--- files/webreader.sh
+++ files/webreader.sh
@@ -3,10 +3,16 @@
 
 BASE_DIR=/var/www
 
-[[ $# -eq 1 ]] || exit 1
+[ $# -eq 1 ] || exit 1
+
 
 WEBREADER=$1
 DOCROOT=${BASE_DIR}/${WEBREADER}
+
+[ ! -r ${DOCROOT}/${WEBREADER}.lock ] || exit 0
+
+export PATH=/usr/local/node/node-default/bin:$PATH
+export CI=true
 
 #Main
 cd $DOCROOT
